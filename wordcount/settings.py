7dfed220f96ce7f1e6cd921a42297f44c0c53249
Aishika Midder <aishikamidder50@gmail.com>
2020-08-31 17:38:47 +0530

    made changes

--- wordcount/settings.py
+++ wordcount/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '2)x$$9z=9f(a3fz$73v=w+-ui8cstvhycqa%a&05*g-h2rqp84'
+SECRET_KEY = 'SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
